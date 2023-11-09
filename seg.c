be27eb6db40e4be51e4c9c6057a5f801ad74865a
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2023-11-09 18:45:29 -0500

    first commit

---
+++ seg.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//This is a segfault app
+int main() {
+    int* ptr = NULL; // Null pointer
+    printf("About to dereference null pointer, expect a segfault.\n");
+    *ptr = 10; // Dereferencing null pointer causes segfault
+    return 0;
+}
+
